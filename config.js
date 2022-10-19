ba77f493f5e720d4797476080fc00ca6aa864af0
zrtlxx <110093701+zrtlxx@users.noreply.github.com>
2022-10-19 13:24:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE ZR BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281223084340','6281223084340','6281223084340']
+global.premium = ['6281223084340']
+global.packname = 'GAK TAHU'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
